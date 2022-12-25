1f9a95ca521a8ad64b43a0ca8f9a70565829441b
pokytester <fellipe.patrick678@gmail.com>
2022-12-25 10:44:55 -0300

    Adicionando arquivos

---
+++ src/ModuloUm/Atividade01.java
@@ -0,0 +1,16 @@
+package ModuloUm;
+
+public class Atividade01 {
+    public static void main(String[] args) {
+        System.out.println("Calculando o valor da Area de Um Circulo");
+        /*
+         * final serve para definir o valor final da variavel no codigo
+         *
+         */
+        double raio = 5;
+        final double pi = 3.14;
+        double resultado = pi * (raio * raio);
+        System.out.println(resultado);
+
+    }
+}
