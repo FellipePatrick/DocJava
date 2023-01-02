20e217a1739099445dcf63ea96917c5999aaa606
pokytester <fellipe.patrick678@gmail.com>
2023-01-02 14:00:50 -0300

    adicionando

---
+++ src/ModuloUm/atividade02.java
@@ -0,0 +1,14 @@
+package ModuloUm;
+
+public class atividade02 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        double n = 5.0 / 9.0;
+        double constante = 32;
+        double valorF = 86;
+        double valorC = (valorF - constante) * n;
+        System.out.println(valorC + "°C Graus Celsius");
+    }
+}
